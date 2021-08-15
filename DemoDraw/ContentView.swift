91304ca7c00c38e8868c8ee2070ed4ac49878fb0
ting3242006 <ting3242006@hotmail.com>
2021-08-15 14:44:37 +0800

    Initial Commit

---
+++ DemoDraw/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DemoDraw
+//
+//  Created by Ting on 2021/8/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
